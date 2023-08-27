f07350495495c0ece58ca15b693e5275ba97644d
unknown <409003604@qq.com>
2023-08-27 20:04:35 +0800

    修改主题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'LiteShell',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -69,7 +69,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'LiteShell',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
