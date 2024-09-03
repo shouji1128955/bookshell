956747b5bc0a85082ff1796bc57c6f06de8604c6
zhanglaiqiang <409003604@qq.com>
2024-09-04 01:34:40 +0800

    delete affinty error link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-	    to="/docs/Kubernetes/1.operator.md">
+	    to="/docs/Kubernetes">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
