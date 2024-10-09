8dd4fe2ce60be7e9ecd91a8fc329600ab59fe0e3
zhanglaiqiang <laiqiang.zhang@aminer.cn>
2024-10-10 01:24:18 +0800

    add domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://shouji1128955.github.io/',
+  url: 'https://book.onlinebookshell.co.in/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/bookshell',
